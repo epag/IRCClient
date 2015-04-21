6461582cd87d9343907d94d6beb160f802b13e3b
Evan Pagryzinski <epagryzi@purdue.edu>
2015-04-21 13:09:56 -0400

    Make IRCclient

--- IRCClient.cc
+++ IRCClient.cc
@@ -119,6 +119,9 @@ int main( int   argc,
     gtk_widget_show (table);
 
     // Add list of rooms. Use columns 0 to 4 (exclusive) and rows 0 to 4 (exclusive)
+    list_rooms = gtk_list_store_new (1, G_TYPE_STRING);
+    update_list_rooms();
+    list = create_list ("Rooms", list_rooms);
     gtk_table_attach_defaults (GTK_TABLE (table), list, 2, 4, 0, 2);
     gtk_widget_show (list);
    
