e5548eabfa9948cfabded473728029ff81b2808e
Evan Pagryzinski <epagryzi@purdue.edu>
2015-04-25 15:19:43 -0400

    Make IRCclient

--- IRCClient.cc
+++ IRCClient.cc
@@ -210,14 +210,14 @@ void logOn_clicked (GtkWidget *widget, gpointer data) {
     gtk_text_buffer_get_start_iter(buffer, &start);
     gtk_text_buffer_get_end_iter(buffer, &end);
     gchar*  name = (char *) gtk_text_buffer_get_text(buffer, &start, &end, false);
-    user_name = name;
+    name = name;
 
     GtkTextIter start2, end2;
     gtk_text_buffer_get_start_iter(passwordBuffer, &start2);
     gtk_text_buffer_get_end_iter(passwordBuffer, &end2);
     gchar* password = (char *) gtk_text_buffer_get_text(passwordBuffer, &start2, &end2, false);
     g_print("%s %s\n", name, password);
-    user_password = password;
+    password = password;
     
     add_user();
 
