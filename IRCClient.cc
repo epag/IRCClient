e535bc4c42c6b81c493bfe2e21a05e0d50ea55e1
Evan Pagryzinski <epagryzi@purdue.edu>
2015-04-25 20:27:18 -0400

    Make IRCclient

--- IRCClient.cc
+++ IRCClient.cc
@@ -160,7 +160,7 @@ void update_list_rooms() {
 
     /* Add some messages to the window */
     for (i = 0; i < RoomNumber; i++) {
-        char * msg = "WHY?!";
+        gchar * msg = "WHY?!";
         gtk_list_store_append (GTK_LIST_STORE (list_rooms), &iter);
         gtk_list_store_set (GTK_LIST_STORE (list_rooms), &iter, 0, msg,-1);
     }
