902590dc958355672ed0d7d08286f6299e16193a
Evan Pagryzinski <epagryzi@purdue.edu>
2015-04-27 17:32:02 -0400

    Make IRCclient

--- IRCClient.cc
+++ IRCClient.cc
@@ -222,13 +222,11 @@ void get_messages(char * room) {
     messages = create_text(sentMessage);
     gtk_table_attach_defaults (GTK_TABLE (table), messages, 0, 4, 2, 5);
     gtk_widget_show (messages);*/
-    if (strcmp(responce, "NO-NEW-MESSAGES\r\n")) {
-        if (msgNum != 0) {
-    msgNum--;
-        }
-    get_messages(room);
-    insert_text (chatLog, responce);
-    msgNum++;
+    if (!strcmp(responce, "NO-NEW-MESSAGES\r\n")) {
+        msgNum--;
+        get_messages(room);
+        insert_text (chatLog, responce);
+        msgNum++;
     if (msgNum == 100) {
         msgNum--;
     }
