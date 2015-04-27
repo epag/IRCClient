563139a58ff7ce3458c7b67fe9f08a5728fab7c8
Evan Pagryzinski <epagryzi@purdue.edu>
2015-04-27 15:37:27 -0400

    Make IRCclient

--- IRCClient.cc
+++ IRCClient.cc
@@ -229,7 +229,7 @@ void get_messages(char * room) {
     char responce [MAX_RESPONCE];
     char * num = "0";
     sendCommand2 (host, port, "GET-MESSAGES2", user, password, num, room, responce);
-
+    gtk_widget_destroy(messages);
     sentMessage = responce;
     messages = create_text(sentMessage);
     gtk_table_attach_defaults (GTK_TABLE (table), messages, 0, 4, 2, 5);
